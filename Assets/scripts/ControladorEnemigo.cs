6c7e0c2bcc5471499ddf00b1c5190553c06fe478
Yosadhara <yosadhara@live.com.mx>
2021-06-17 13:11:53 -0500

    se agrega script nuevo

--- Assets/scripts/ControladorEnemigo.cs
+++ Assets/scripts/ControladorEnemigo.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class move : MonoBehaviour
+public class ControladorEnemigo : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
