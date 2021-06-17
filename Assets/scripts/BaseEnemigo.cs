91d765a3936323c3320c78f7590299a4763d8072
Yosadhara <yosadhara@live.com.mx>
2021-06-17 15:30:35 -0500

    se agrego codigos en enemigo

--- Assets/scripts/BaseEnemigo.cs
+++ Assets/scripts/BaseEnemigo.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class BaseEnemigo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+     public ConstructorEnemigo[] nuevoJugador;
 
     // Update is called once per frame
-    void Update()
+    void Awake()
     {
-        
+        nuevoJugador = new ConstructorEnemigo[2];
+
+        nuevoJugador[0] = new ConstructorEnemigo(1, "Sr.Pato", 10,6);
+
+        nuevoJugador[1] = new ConstructorEnemigo(2, "Leopardo Blanco", 20,5);
+    
     }
 }
