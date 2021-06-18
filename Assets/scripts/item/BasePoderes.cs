ea1a79346a22deb3f4c74c283604b7e8abdeed9c
Yosadhara <yosadhara@live.com.mx>
2021-06-17 22:26:00 -0500

    se agregaron y crearon scripts y codigos de poderes

---
+++ Assets/scripts/item/BasePoderes.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasePoderes : MonoBehaviour
+{
+    public ConstructorPoderes[] nuevoPoder;
+
+    void Awake()
+    {
+        nuevoPoder = new ConstructorPoderes[3];
+        nuevoPoder[0] = new ConstructorPoderes(1, "bambu");
+        nuevoPoder[1] = new ConstructorPoderes(2, "monedas");
+        nuevoPoder[2] = new ConstructorPoderes(3, "posion");
+    }
+}
---
+++ Assets/scripts/item/ConstructorPoderes.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ConstructorPoderes : MonoBehaviour
+{
+    public int poderId;
+    public string nombrePoder;
+
+    public ConstructorPoderes(int id, string x)
+    {
+        this.poderId =id;
+        this.nombrePoder =x;
+    }
+}
